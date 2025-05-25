3dd04022e36b2a96bf5b8b625ccea691a322c8e0
Marc Chéhab <marc@luzmedia.ch>
2025-05-25 17:07:44 +0200

    auth conditional, should be easier to develop without environment variables.

--- packages/shared/lib/Providers.tsx
+++ packages/shared/lib/Providers.tsx
@@ -4,7 +4,20 @@ import { SessionProvider } from 'next-auth/react'
 import { TurtleProvider } from '../components/TurtleEditor/TurtleContext'
 import { ReactNode } from 'react'
 
+const isAuthConfigured = !!(process.env.NEXTAUTH_SECRET && process.env.POSTGRES_URL)
+
 export default function Providers({ children }: { children: ReactNode }) {
+
+    if (!isAuthConfigured) {
+        return (
+            <SessionProvider session={null}>
+                <TurtleProvider>
+                    {children}
+                </TurtleProvider>
+            </SessionProvider>
+        )
+    }
+
     return (
         <SessionProvider>
             <TurtleProvider>
